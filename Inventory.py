8ee0c40558ae3fbd3037f6d1862f52d8b23b4574
postfen <26173007+postfen@users.noreply.github.com>
2022-01-03 19:45:56 -0700

    Formatting/test again

--- Inventory.py
+++ Inventory.py
@@ -1,4 +1,4 @@
-'''
+"""
 Kitchen Inventory - a grocery package manager.
 ================================================================================
 
@@ -50,6 +50,8 @@ while inventory == False:
                       Asks user for kitchen to delete, finds kitchen, asks for
                       confirmation of deletion.
 
+
+
 Kitchen Chosen  -> 
                  Submenu for Kitchen:
                                      Remove Items
@@ -74,108 +76,116 @@ Kitchen Chosen  ->
 
 
 Add Kitchen -> Asks user for name, then creates Kitchen object with that name.
-'''
+"""
+
 
 def start_up_menu():
-    '''
+    """
     Start up menu to create Kitchen, or Choose Kitchen to view/manipulate.
-    '''
+    """
     while True:
         return
-        
-    
-    
-    
-    
+
+
 def menu():
-    '''
+    """
     Main User menu for manipulating kitchen inventory
-    '''
+    """
     while True:
-        print ("A)\tRemove Items")
-        print ("R)\tRemove Items")
-        print ("F)\tFind Items")
-        print ("L)\tList all Items")
-        print ("D)\tDisplay Shopping List")
-        print ("Q)\tQuit")
-        print ()
+        print("A)\tRemove Items")
+        print("R)\tRemove Items")
+        print("F)\tFind Items")
+        print("L)\tList all Items")
+        print("D)\tDisplay Shopping List")
+        print("Q)\tQuit")
+        print()
         answer = input(str("Enter your choice: ")).upper()
-        if len(answer) == 1 and answer in "ARFLQ": # Input validation for menu
-          return(answer)
+        if len(answer) == 1 and answer in "ARFLQ":  # Input validation for menu
+            return answer
+
 
 def sub_menu(menu):
-    '''
+    """
     User menu for manipulating items in each location
-    '''
+    """
     while True:
-        print(f'You are in the {menu} menu')
-        print ('1) Add Item')
-        print ('2) Remove Item')
-    
-def main():
-  '''
-  Main program loop, starts menu function, accesses all other funcitons
-  through user input.
-  
-  NEED TO SETUP WAY FOR PROGRAM TO SAVE SPECIFIC KITCHEN OBJECTS TO FILE:
-  FOR NOW ITS INITIALIZING A NEW KITCHEN EVERY TIME.
-  
-#===============================================================================
-  
-  '''
-  kitchen_database = Kitchen() # Initializing house database
-  choice = -1 # Placeholder for menu while loop state variable
+        print(f"You are in the {menu} menu")
+        print("1) Add Item")
+        print("2) Remove Item")
 
-  while choice != 'Q':  # While user input isn't 'q' or 'Q', keep menu going
+
+def main():
+    """
+      Main program loop, starts menu function, accesses all other funcitons
+      through user input.
+
+      NEED TO SETUP WAY FOR PROGRAM TO SAVE SPECIFIC KITCHEN OBJECTS TO FILE:
+      FOR NOW ITS INITIALIZING A NEW KITCHEN EVERY TIME.
+
+    #===============================================================================
+
+    """
+    # We could add a database object that holds kitchens?
+    # Perhaps that would get too messy though. I'm just thinking of a way to
+    # print out all kitchens, or make sure each kitchen is separated but
+    # kept in a database.
+
+    kitchen_database = Kitchen()  # Initializing house database
+    choice = -1  # Placeholder for menu while loop state variable
+
+    while choice != "Q":  # While user input isn't 'q' or 'Q', keep menu going
         choice = menu()
 
-        if choice == 'A': # If user inputs 'a' or 'A', start addHouse fnctn
-            kitchen_database += food_item() # food init runs through food object 
-            print()                    # creation, then += adds to the database
-            
-            # Not perfect, 
-          
-        elif choice == 'R': # if user inputs 'r' or 'R', start remove_food fnctn
+        if choice == "A":  # If user inputs 'a' or 'A', start addHouse fnctn
+            kitchen_database += (
+                food_item()
+            )  # food init runs through food object
+            print()  # creation, then += adds to the database
+
+            # Not perfect,
+
+        elif (
+            choice == "R"
+        ):  # if user inputs 'r' or 'R', start remove_food fnctn
             kitchen_database._remove_from_database()
             print()
 
-        elif choice == 'F': # if user inputs 'f' or 'F', start findhouse fnctn
-            result = house_database._findHouse('f')
-            if not not result: # if function doesn't return False/if house found
+        elif choice == "F":  # if user inputs 'f' or 'F', start findhouse fnctn
+            result = house_database._findHouse("f")
+            if (
+                not not result
+            ):  # if function doesn't return False/if house found
                 print(result)  # print house object
             print()
 
-        elif choice == 'L': # if L, print out the house database.
+        elif choice == "L":  # if L, print out the house database.
             print()
-            print(house_database)
-        
-        
-        elif choice == 'Q':
+            # List of Food Items in Kitchen Database,
+            # or location of ktcn
+
+        elif choice == "Q":
             print("Program exited.")
             return
-        else: print("Invalid Input, Try Again")
+        else:
+            print("Invalid Input, Try Again")
 
 
-
-
-class Kitchen(self):
+class Kitchen:
     def __init__(self):
-        self.inventory = [self.fridge, self.freezer, self.pantry]
-        self.fridge = {}
-        self.freezer = {}
-        self.pantry = {}
-    
-    def _addFridge(self):
-        
-        
+        self.inventory = {}
+
+    def _addFood(self):
+        return
+
+    def _searchFood(self):
+        return
+
+    def _removeFood(self):
+        return
 
 
-    def _addFridge(self):
-
-
-
-    
-    
-        
-class Refridgerator(self):
-    def __init__(self):
+class food_item:
+    def __init__(self, location, quantity, essential=False):
+        self.location = location
+        self.quantity = quantity
+        self.essential = essential
